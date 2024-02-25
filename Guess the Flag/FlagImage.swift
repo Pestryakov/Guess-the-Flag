b260aba2e4435c7f6a86448b492ccc467bf062fa
Maxim <pestriakovm@gmail.com>
2024-02-25 15:48:39 -0300

    4th commit(new FlagImage view, custom ViewModifier)

---
+++ Guess the Flag/FlagImage.swift
@@ -0,0 +1,18 @@
+//
+//  FlagImage.swift
+//  Guess the Flag
+//
+//  Created by Maxim P on 25/02/2024.
+//
+
+import SwiftUI
+
+struct FlagImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlagImage()
+}
